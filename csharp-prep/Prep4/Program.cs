5366ac2492ec89b254c0e8581447773489ed3d1a
JamesOnGitHub54 <jamesg521@outlook.com>
2024-11-09 06:44:19 -0700

    Completed Prep4, Prep5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int userNumber = -1;
+        List<int> numbers = new List<int>();
+
+        //Get Numbers
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+
+
+        } while (userNumber != 0);
+
+        //Calculations
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+
+        float totNumbers = numbers.Count;
+        float average = sum / totNumbers;
+
+
+        int largest = 0;
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        //Results
+        Console.WriteLine($"The sum is: {sum} \nThe average is: {average} \nThe largest number is: {largest}");
     }
 }
